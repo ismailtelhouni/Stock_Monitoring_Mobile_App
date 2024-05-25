59136548dcdc8cff2a478786bcbc9af7f89ed55b
Yass1509 <yechchykry@gmail.com>
2024-05-25 16:48:53 +0100

    notification 1

---
+++ app/src/main/java/com/example/stock/MyFirebaseMessagingService.java
@@ -0,0 +1,49 @@
+package com.example.stock;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import com.example.stock.R;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+        getFirebaseMessage(remoteMessage.getNotification().getTitle(), remoteMessage.getNotification().getBody());
+
+    }
+
+    public void getFirebaseMessage(String title, String msg) {
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "myFirebaseChannel")
+                .setSmallIcon(R.drawable.baseline_notifications_active_24)
+                .setContentTitle(title)
+                .setContentText(msg)
+                .setAutoCancel(true);
+
+
+        NotificationManagerCompat manager = NotificationManagerCompat.from(this);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        manager.notify(101, builder.build());
+
+    }
+}
+
+
