de1f50a973cd7eab3f9f7593d17afdffdf903ac7
nacefzakaria <zakaria_nacef@um5.ac.ma>
2024-05-26 01:42:10 +0100

    generate xml to pdf

--- app/src/main/java/com/example/stock/activity/LoginActivity.java
+++ app/src/main/java/com/example/stock/activity/LoginActivity.java
@@ -1,10 +1,21 @@
 package com.example.stock.activity;
 
+import android.Manifest;
 import android.content.Intent;
+import android.graphics.Canvas;
+
+import android.graphics.pdf.PdfDocument;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.text.InputType;
 import android.text.TextUtils;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ProgressBar;
@@ -14,6 +25,7 @@ import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -22,6 +34,12 @@ import com.example.stock.R;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import android.graphics.Bitmap;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class LoginActivity extends AppCompatActivity  implements View.OnClickListener {
 
@@ -31,6 +49,8 @@ public class LoginActivity extends AppCompatActivity  implements View.OnClickLis
     ProgressBar progressBar;
     RelativeLayout itemVisibility;
     ImageButton btnShowPassword , btnHidePassword ;
+    final static int REQUEST_CODE = 1234;
+    Button ButtonCreatePDF;
     @Override
     protected void onStart() {
         super.onStart();
@@ -69,6 +89,16 @@ public class LoginActivity extends AppCompatActivity  implements View.OnClickLis
         buttonLogin.setOnClickListener(this);
         btnHidePassword.setOnClickListener(this);
         btnShowPassword.setOnClickListener(this);
+        askForPermissions();
+        ButtonCreatePDF = findViewById(R.id.generatePdfButton);
+
+
+        ButtonCreatePDF.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                createPDF();
+            }
+        });
 
     }
 
@@ -142,4 +172,55 @@ public class LoginActivity extends AppCompatActivity  implements View.OnClickLis
         progressBar.setVisibility(View.GONE);
         itemVisibility.setVisibility(View.VISIBLE);
     }
+    private void askForPermissions(){
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_CODE);
+    }
+
+    public void createPDF(){
+        View view = LayoutInflater.from(this).inflate(R.layout.activity_login, null);
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R){
+            this.getDisplay().getRealMetrics(displayMetrics);
+
+        }
+        else this.getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+
+        view.measure(View.MeasureSpec.makeMeasureSpec(displayMetrics.widthPixels, View.MeasureSpec.EXACTLY ),
+                View.MeasureSpec.makeMeasureSpec(displayMetrics.widthPixels, View.MeasureSpec.EXACTLY));
+
+        view.layout(0, 0, displayMetrics.widthPixels, displayMetrics.heightPixels);
+
+        PdfDocument document = new PdfDocument();
+        int viewWidth = view.getMeasuredWidth();
+        int viewHeight = view.getMeasuredHeight();
+
+        PdfDocument.PageInfo pageInfo = new PdfDocument.PageInfo.Builder(viewWidth, viewHeight,1).create();
+        PdfDocument.Page page = document.startPage(pageInfo);
+
+
+
+        Canvas canvas = page.getCanvas();
+        view.draw(canvas);
+        document.finishPage(page);
+
+        File downloadsDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+        String fileName = "exempleXML.pdf";
+        File file = new File(downloadsDir, fileName);
+        try {
+            FileOutputStream fos = new FileOutputStream(file);
+            document.writeTo(fos);
+            document.close();
+            fos.close();
+            Toast.makeText(this, "Writing succefully ",Toast.LENGTH_SHORT).show();
+        } catch (FileNotFoundException e) {
+            Log.d("mylog", "Error while writing" + e.toString());
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+
 }
